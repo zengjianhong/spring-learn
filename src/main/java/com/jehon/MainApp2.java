d956837a085f5d5820c34a8d36e2b2239762c631
zengjianhong <771970504@qq.com>
2021-09-14 08:38:33 +0800

    commit

--- src/main/java/com/jehon/MainApp2.java
+++ src/main/java/com/jehon/MainApp2.java
@@ -5,6 +5,11 @@ import com.jehon.definition.DefineIndia;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 /**
  * @author jehon
  */
@@ -21,5 +26,25 @@ public class MainApp2 {
         defineIndia.getMessage1();
         defineIndia.getMessage2();
         defineIndia.getMessage3();
+
+        HashMap hashMap = new HashMap();
+        System.out.println(hashMap.put(null, null));
+        System.out.println(hashMap.put(null, null));
+        System.out.println(hashMap.put(null, null));
+        System.out.println(hashMap.get(null));
+        System.out.println(hashMap.size());
+
+        HashSet hashSet = new HashSet<String>();
+        System.out.println(hashSet.add(null));
+        System.out.println(hashSet.add(null));
+        System.out.println(hashSet.add("1"));
+        System.out.println(hashSet.add("1"));
+
+//        TreeMap<String, String> treeMap = new TreeMap<>();
+//        System.out.println(treeMap.put(null, null));
+//        treeMap.put(null, null);
+
+//        TreeSet<String> treeSet = new TreeSet<>();
+//        System.out.println(treeSet.add(null));
     }
 }
